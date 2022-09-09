5c6d728bc4e846656dbfe56c77b4df0b0d3bd77c
takykongo <takykongoabdoul@gmail.com>
2022-09-09 18:03:14 +0000

    If else - if else

--- 30DaysOfJavascript/04-Day/main.js
+++ 30DaysOfJavascript/04-Day/main.js
@@ -29,3 +29,15 @@ if (isRaining) {
     console.log('No need for a rain coat.')
 }
 // No need for a rain coat.
+
+// if else if else
+let a=0
+if(a > 0){
+    console.log(`${a} is a positive number`)
+}else if(a < 0) {
+    console.log(`${a} is a negative number`)
+}else if(a == 0) {
+    console.log(`${a} is zero`)
+}else {
+    console.log(`${a} is not a number`)
+}

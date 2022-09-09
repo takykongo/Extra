8e0b7c59e5ac8b669a68fe9ad94678ff3c6e67a7
takykongo <takykongoabdoul@gmail.com>
2022-09-09 18:04:45 +0000

    If else - if else

--- 30DaysOfJavascript/04-Day/main.js
+++ 30DaysOfJavascript/04-Day/main.js
@@ -41,3 +41,15 @@ if(a > 0){
 }else {
     console.log(`${a} is not a number`)
 }
+
+// if else if else
+let weather = 'sunny'
+if (weather === 'rainy') {
+  console.log('You need a rain coat.')
+} else if (weather === 'cloudy') {
+  console.log('It might be cold, you need a jacket.')
+} else if (weather === 'sunny') {
+  console.log('Go out freely.')
+} else {
+  console.log('No need for rain coat.')
+}

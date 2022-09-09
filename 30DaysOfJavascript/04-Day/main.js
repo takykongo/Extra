00fa79f75307aaf0b9af78951c7c5fa3afde107d
takykongo <takykongoabdoul@gmail.com>
2022-09-09 17:51:12 +0000

    Conditional If - else

---
+++ 30DaysOfJavascript/04-Day/main.js
@@ -0,0 +1,31 @@
+// if
+let num = 3
+if(num > 0){
+console.log(`${num} is a positive number`)}
+// else
+else{
+    console.log(`${num} is a negative number`)
+}
+
+num = -3
+if(num > 0){
+    console.log(`${num} is a positive number`)
+}else{
+    console.log(`${num} is a negative number`)
+}
+
+let isRaining = true
+if (isRaining) {
+    console.log('You need a rain coat.')
+} else {
+    console.log('No need for a rain coat.')
+}
+// You need a rain coat.
+  
+isRaining = false
+if (isRaining) {
+    console.log('You need a rain coat.')
+} else {
+    console.log('No need for a rain coat.')
+}
+// No need for a rain coat.

045b4f7b6da2b6ab452ef2d31deec93e75d10767
takykongo <takykongoabdoul@gmail.com>
2022-09-10 14:39:47 +0000

    Exercice_3

--- 30DaysOfJavascript/04-Day/Exercice_3/main.js
+++ 30DaysOfJavascript/04-Day/Exercice_3/main.js
@@ -14,3 +14,21 @@ if((months == 'january')||(months == 'march')||(months == 'may')||(months == 'ju
 }else{
     console.log(`Enter a month.!`)
 }
+
+// 2- Write a program which tells the number of days in a month, now consider leap year.
+
+month = prompt('Enter a month: ')
+months = month.toLowerCase()
+
+if((months == 'january')||(months == 'march')||(months == 'may')||(months == 'july')||(months == 'august')||(months == 'october')||(months == 'december'))
+{
+    console.log(`${month} has 31 days.`)
+}else if((months == 'april')||(months == 'june')||(months == 'september')||(months == 'november'))
+{
+    console.log(`${month} has 30 days.`)
+}else if(months == 'february')
+{
+    console.log(`${month} has 29 days.`)
+}else{
+    console.log(`Enter a month.!`)
+}

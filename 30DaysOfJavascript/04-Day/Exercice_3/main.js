285d11ce6b715313f1023e5318caff8355a6aa6f
takykongo <takykongoabdoul@gmail.com>
2022-09-10 14:37:38 +0000

    Exercice_3

---
+++ 30DaysOfJavascript/04-Day/Exercice_3/main.js
@@ -0,0 +1,16 @@
+//1- Write a program which tells the number of days in a month.
+let month = prompt('Enter a month: ')
+let months = month.toLowerCase()
+
+if((months == 'january')||(months == 'march')||(months == 'may')||(months == 'july')||(months == 'august')||(months == 'october')||(months == 'december'))
+{
+    console.log(`${month} has 31 days.`)
+}else if((months == 'april')||(months == 'june')||(months == 'september')||(months == 'november'))
+{
+    console.log(`${month} has 30 days.`)
+}else if(months == 'february')
+{
+    console.log(`${month} has 28 days.`)
+}else{
+    console.log(`Enter a month.!`)
+}
